7f40335db1163506aefee7304785e5b9157dc6d7
Petter Daae <petter.daae@gmail.com>
2020-10-18 14:49:20 +0200

    More parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,24 +18,54 @@ func parseExpected(t *testing.T, program string, expected grammar.Exp) {
 
 func TestSimplePlus(t *testing.T) {
 	str := "1 + 2"
-	expected := grammar.ExpPlus{Left: grammar.ExpNum{Value: 1}, Right: grammar.ExpNum{Value: 2}}
+	expected := grammar.ExpPlus{
+		Left:  grammar.ExpNum{Value: 1},
+		Right: grammar.ExpNum{Value: 2},
+	}
 	parseExpected(t, str, expected)
 }
 
 func TestSimpleMultiply(t *testing.T) {
 	str := "1 * 2"
-	expected := grammar.ExpMultiply{Left: grammar.ExpNum{Value: 1}, Right: grammar.ExpNum{Value: 2}}
+	expected := grammar.ExpMultiply{
+		Left:  grammar.ExpNum{Value: 1},
+		Right: grammar.ExpNum{Value: 2},
+	}
 	parseExpected(t, str, expected)
 }
 
 func TestSimpleParentheses(t *testing.T) {
 	str := "( 1 )"
-	expected := grammar.ExpParentheses{Inside: grammar.ExpNum{Value: 1}}
+	expected := grammar.ExpParentheses{
+		Inside: grammar.ExpNum{Value: 1},
+	}
 	parseExpected(t, str, expected)
 }
 
 func TestParenthesesInPlusExpression(t *testing.T) {
 	str := "( 1 + 2 ) + 3"
-	expected := grammar.ExpPlus{Left: grammar.ExpParentheses{grammar.ExpPlus{Left: grammar.ExpNum{1}, Right: grammar.ExpNum{Value: 2}}}, Right: grammar.ExpNum{Value: 3}}
+	expected := grammar.ExpPlus{
+		Left: grammar.ExpParentheses{
+			Inside: grammar.ExpPlus{
+				Left:  grammar.ExpNum{1},
+				Right: grammar.ExpNum{Value: 2},
+			},
+		},
+		Right: grammar.ExpNum{Value: 3},
+	}
+	parseExpected(t, str, expected)
+}
+
+func TestBinaryExpressionWithTrailingParentheses(t *testing.T) {
+	str := "1 * ( 2 * 3)"
+	expected := grammar.ExpMultiply{
+		Left: grammar.ExpNum{Value: 1},
+		Right: grammar.ExpParentheses{
+			Inside: grammar.ExpMultiply{
+				Left:  grammar.ExpNum{Value: 2},
+				Right: grammar.ExpNum{Value: 3},
+			},
+		},
+	}
 	parseExpected(t, str, expected)
 }
