e63f6008dff15175d5c1203a36ea4005e561c58a
Petter Daae <petter.daae@gmail.com>
2020-12-06 13:39:01 +0100

    Continue context refactoring

--- language/assembly/contexts.go
+++ language/assembly/contexts.go
@@ -19,6 +19,13 @@ type context struct {
 	procedures               map[string]string
 	stack                    map[string]int
 	propagateChangesToParent bool
+	procedure                *procedure
+}
+
+type procedure struct{
+	name string
+	operations []string
+	stackSizeWhenCreated int
 }
 
 func (contexts Contexts) Push(context *context) {
@@ -47,14 +54,89 @@ func (contexts Contexts) Pop() *context {
 	return result
 }
 
-func (contexts Contexts) Get(name string, stackSize int, procedure Procedure) (string, error) {
+func (contexts Contexts) getFromContext(context *context, name string, stackSize int) (ContextElementType, string, error) {
+	if context == nil {
+		return Invalid, "", fmt.Errorf("context stack is empty")
+	}
+
+	procedure := contexts.getParentProcedure()
+
+	stack, ok := context.stack[name]
+	if ok {
+		diff := (stackSize - stack) * 8
+
+		if procedure != nil {
+			if stack > procedure.stackSizeWhenCreated {
+				return Stack, fmt.Sprintf("[rsp+%d]", diff), nil
+			}
+
+			return Stack, fmt.Sprintf("[rsp+rcx+%d+8]", diff), nil
+		}
+
+		return Stack, fmt.Sprintf("[rsp+%d]", diff), nil
+	}
+
+	proc, ok := context.procedures[name]
+	if ok {
+		return Procedure, proc, nil
+	}
+
+	return Invalid, "", fmt.Errorf("could not get '%s' from context", name)
+}
+
+func (contexts Contexts) getParentProcedure() *procedure {
+	for i := contexts.Size() - 1; i >= 0; i-- {
+		current := contexts.stack[i].procedure
+		if current != nil {
+			return current
+		}
+	}
+	return nil
+}
+
+func (contexts Contexts) Get(name string, stackSize int) (ContextElementType, string, error) {
+	context := contexts.Peek()
+	return contexts.getFromContext(context, name, stackSize)
+}
+
+func (contexts Contexts) StackInsert(name string, value string, stackSize int) ([]string, int, error) {
+	var operations []string
+	pushes := 0
 	top := contexts.Peek()
 	if top == nil {
-		return "", fmt.Errorf("context stack is empty")
+		return nil, 0, fmt.Errorf("context stack is empty")
 	}
 
-	stack, ok := top.stack[name]
-	if ok {
-
+	// If name is not in current stack
+	_, ok := top.stack[name]
+	if !ok {
+		operations = append(operations, fmt.Sprintf("push %s", value))
+		pushes++
+		top.stack[name] = stackSize
+		return operations, pushes, nil
 	}
+
+	// If name is in current stack
+	if top.procedure == nil {
+		for i := contexts.Size() - 1; i >= 0; i-- {
+			current := contexts.stack[i]
+			_, ok := current.stack[name]
+			if !ok {
+				break
+			}
+
+			_, address, _ := contexts.getFromContext(current, name, stackSize)
+			operations = append(operations, fmt.Sprintf("mov %s, %s", address, value))
+
+			if current.procedure != nil {
+				break
+			}
+		}
+	}
+
+	return operations, pushes, nil
+}
+
+func (contexts Contexts) ProcInsert(name string, alias string) error {
+	return nil
 }
--- language/assembly/syntax.go
+++
@@ -1,5 +0,0 @@
-package assembly
-
-type Syntax struct {
-
-}
